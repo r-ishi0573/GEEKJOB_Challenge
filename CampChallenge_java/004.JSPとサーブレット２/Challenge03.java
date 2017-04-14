191b4bd554638401b8a3f794a313604276d5dc55
R-ishi0573 <sokuroni.1930@gmail.com>
2017-04-14 14:19:55 +0900

    Java/JSPとサーブレット2　引数1　の課題を提出します

---
+++ CampChallenge_java/004.JSPとサーブレット２/Challenge03.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Random;
+
+public class Challenge03 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>奇数・偶数の判定</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            
+            //奇偶数の判定
+            for(int i=1; i<10; i++) {
+                out.print(i+"：");
+                numJudge(out, i);
+                out.print("<br>");
+            }
+            
+            //out.println("<h1>今日のあなたの運勢は。。。 " + lucklist[index] + "！</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }finally {
+            out.close();
+        }
+    }
+    
+    /*
+    　課題：奇偶数判定メソッド
+    */
+    void numJudge(PrintWriter out, int num) {
+        if( (num % 2) != 0 ) {
+            out.println("奇数です");
+        }
+        else {
+            out.println("偶数です");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
